927d2e52a9b037203a283a9bb4f5a8d2a087e3c4
harveycdr <harvey.hau4502@gmail.com>
2025-08-20 22:29:47 +0700

    fix-video-player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,37 +6,25 @@ export default function Player({ src, poster }) {
 
   useEffect(() => {
     const video = videoRef.current;
+    if (!video || !src) return;
 
-    if (!video) return;
-
-    // Nếu trình duyệt hỗ trợ natively (Safari)
     if (video.canPlayType("application/vnd.apple.mpegurl")) {
       video.src = src;
     } else if (Hls.isSupported()) {
-      // Chrome, Firefox, Edge...
       const hls = new Hls();
       hls.loadSource(src);
       hls.attachMedia(video);
-
-      hls.on(Hls.Events.ERROR, function (event, data) {
-        console.error("HLS error:", data);
-      });
-
-      return () => {
-        hls.destroy();
-      };
-    } else {
-      console.error("Trình duyệt không hỗ trợ HLS");
+      return () => hls.destroy();
     }
   }, [src]);
 
   return (
-    <div className="w-200 h-100 mx-auto bg-black rounded-2xl overflow-hidden shadow-lg flex justify-center">
+    <div className="w-full h-70 xl:h-120 bg-black rounded-2xl overflow-hidden flex justify-center">
       <video
         ref={videoRef}
-        controls
         poster={poster}
-        className="w-200 max-h-100"
+        controls
+        className="w-full max-h-70 xl:max-h-120 object-contain"
       />
     </div>
   );
